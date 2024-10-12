cca74e2ba62b2345dfb95c4096ccdfb4c5e42ae0
Kevin Cui <106274250+kdotcui@users.noreply.github.com>
2024-10-12 09:36:57 -0400

    Refactored modal and added user location chagnes

--- UniRoute/UniRoute/MapView.swift
+++ UniRoute/UniRoute/MapView.swift
@@ -6,23 +6,59 @@
 //
 
 import SwiftUI
-import GoogleMaps
+import MapKit
 
-struct MapView: UIViewRepresentable {
+struct MapView: View {
+    @StateObject private var viewModel = MapViewModel()
     
-    @ObservedObject var locationManager = LocationManager()
-    
-    func makeUIView(context: Context) -> GMSMapView {
-        let camera = GMSCameraPosition.camera(withLatitude: locationManager.lastLocation?.latitude ?? -33.86, longitude: locationManager.lastLocation?.longitude ?? 151.20, zoom: 15.0)
-        let mapView = GMSMapView.map(withFrame: CGRect.zero, camera: camera)
-        mapView.isMyLocationEnabled = true // Show the user's location on the map
-        return mapView
-    }
-    
-    func updateUIView(_ mapView: GMSMapView, context: Context) {
-        if let location = locationManager.lastLocation {
-            mapView.animate(toLocation: CLLocationCoordinate2D(latitude: location.latitude, longitude: location.longitude))
-        }
+    var body: some View {
+        Map(coordinateRegion: $viewModel.region, showsUserLocation: true)
+            .edgesIgnoringSafeArea(.all)
+            .onAppear {
+                viewModel.checkLocationServiceEnabled()
+            }
     }
 }
 
+struct MapView_Previews: PreviewProvider {
+    static var previews: some View {
+        MapView()
+    }
+}
+
+final class MapViewModel: NSObject, ObservableObject, CLLocationManagerDelegate {
+    @Published var region = MKCoordinateRegion(
+        center: CLLocationCoordinate2D(latitude: 42.3601, longitude: -71.0589),
+        span: MKCoordinateSpan(latitudeDelta: 0.05, longitudeDelta: 0.05)
+    )
+    
+    var locationManager: CLLocationManager?
+    
+    func checkLocationServiceEnabled() {
+        if CLLocationManager.locationServicesEnabled() {
+            locationManager = CLLocationManager()
+            locationManager!.delegate = self
+        } else {
+            print("Location is Off. Please turn on!")
+        }
+    }
+    
+    private func checkLocationAuthorization() {
+        switch locationManager?.authorizationStatus {
+            case .notDetermined:
+                locationManager?.requestWhenInUseAuthorization()
+            case .denied, .restricted:
+                print("Location Permissions are Off. Go into settings to turn it on. ")
+        case .authorizedAlways,.authorizedWhenInUse:
+            region = MKCoordinateRegion(
+                center: (locationManager?.location!.coordinate)!,
+                span: MKCoordinateSpan(latitudeDelta: 0.05, longitudeDelta: 0.05))
+            default:
+                break
+        }
+    }
+    
+    func locationManagerDidChangeAuthorization(_ manager: CLLocationManager) {
+        checkLocationAuthorization()
+    }
+}
